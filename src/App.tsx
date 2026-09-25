22e4a3d2c3aee5486917950a16f087302e243ce2
Dylan Meiners <45412038+dylan-meiners@users.noreply.github.com>
2026-09-24 20:24:34 -0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header"></div>
+      <div className="nav"></div>
+      <div className="content"></div>
+    </div>
+  );
+}
+
+export default App;
